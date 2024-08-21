9636a00f347b42433b6edffe93664a35a97a1ae6
Baek <woonn97@gmail.com>
2024-08-21 12:03:05 +0900

    [✨feat]: 자동 줄바꿈(remark-breaks)

--- src/components/md/PostBody.tsx
+++ src/components/md/PostBody.tsx
@@ -3,6 +3,7 @@ import remarkA11yEmoji from '@fec/remark-a11y-emoji';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
+import remarkBreaks from 'remark-breaks';
 import remarkGfm from 'remark-gfm';
 import remarkToc from 'remark-toc';
 
@@ -25,6 +26,7 @@ export const PostBody = ({ children }: Props) => {
             remarkA11yEmoji,
             // 제목을 기반으로 목차를 생성
             remarkToc,
+            remarkBreaks,
           ],
           // 함께 작동하여 ID를 추가하고 제목을 연결
           rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
