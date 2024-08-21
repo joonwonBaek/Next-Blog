eb4d2e893f3361da766860632b916ab2cad4a881
Baek <woonn97@gmail.com>
2024-08-21 11:47:26 +0900

    [✨feat]: NotFound 조회되도록 수정

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -1,23 +1,29 @@
 import { PostBody } from '@/components/md/PostBody';
-import { getPostDetail } from '@/lib/post';
+import { getPostDetail, getPostParamList } from '@/lib/post';
 
 type Props = {
   params: { category: string; slug: string };
 };
 
 // 허용된 param 외 접근시 404
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  const paramList = getPostParamList();
+  return paramList;
+}
 
 const PostDetail = async ({ params }: Props) => {
   const { category, slug } = params;
   const post = await getPostDetail(category, slug);
 
   return (
-    <div className="w-[1000px] mx-auto">
+    <div className="w-[900px] mx-auto">
       <header>
         <h1>{post.title}</h1>
         <div>{post.date.toISOString()}</div>
       </header>
-      <div>
+      <div className="prose max-w-none">
         <PostBody>{post.content}</PostBody>
       </div>
     </div>
