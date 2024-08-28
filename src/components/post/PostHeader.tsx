aa7bf86eb20a018ec7e34c5ebb562c75fddc5219
Baek <woonn97@gmail.com>
2024-08-28 14:43:15 +0900

    [✨feat]: post header에 category 경로 추가

--- src/components/post/PostHeader.tsx
+++ src/components/post/PostHeader.tsx
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import { CalendarDays, Clock3 } from 'lucide-react';
+import Link from 'next/link';
 
 import { Post } from '@/lib/post';
 
@@ -12,7 +13,11 @@ export const PostHeader = ({ post }: Props) => {
   return (
     <header>
       <h1 className="text-center">{post.title}</h1>
-      <div className="text-base"></div>
+      <div className="text-base">
+        <Link href={`/blog/${post.categoryPath}`}>
+          {post.categoryPublicName}
+        </Link>
+      </div>
       <div className="flex justify-center gap-3 text-sm text-gray-500 dark:text-gray-400">
         <div className="flex items-center gap-1">
           <CalendarDays className="w-3.5" />
