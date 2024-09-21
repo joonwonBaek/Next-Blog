36eb460ed038234b648e440d656afbebe4a609c5
Baek <woonn97@gmail.com>
2024-09-21 22:07:13 +0900

    [🐛fix]: baseDomain 수정

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,4 +1,4 @@
-export const baseDomain = 'https://next-blog-azure-xi.vercel.app';
+export const baseDomain = 'https://www.next-blog-azure-xi.vercel.app';
 
 export const blogName = 'BAEK BLOG';
 export const blogDesc = '개발 과정에서 얻은 지식과 경험을 공유합니다.';
