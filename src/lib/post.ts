76d860dda3b059c0ba8469846b2f4ad6b93c5f72
Baek <woonn97@gmail.com>
2024-09-21 16:33:25 +0900

    [🐛fix]: parse toc 규칙 수정

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -159,7 +159,7 @@ export const parseToc = (content: string): HeadingItem[] => {
         heading
           .replace('# ', '')
           .replace('#', '')
-          .replace(/[\[\]:!@#$%^&*()+=,.]/g, '')
+          .replace(/[\[\]:!@#$/%^&*()+=,.]/g, '')
           .replace(/ /g, '-')
           .toLowerCase()
           .replace('?', ''),
