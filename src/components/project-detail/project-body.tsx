ae1fe9ef188763d1885c2991bf2ae9f61959fa52
Baek <woonn97@gmail.com>
2024-11-03 14:12:24 +0900

    [🐛fix]: 배포 에러 수정

--- src/components/project-detail/project-body.tsx
+++ src/components/project-detail/project-body.tsx
@@ -1,6 +1,7 @@
 // @ts-expect-error no types
 import remarkA11yEmoji from '@fec/remark-a11y-emoji';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeSlug from 'rehype-slug';
 import remarkBreaks from 'remark-breaks';
 import remarkGfm from 'remark-gfm';
